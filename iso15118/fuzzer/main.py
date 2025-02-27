6223740742db4e841ad703fec24ad3ad5c069f0c
BoYanZh <boyanzh233@gmail.com>
2025-02-27 01:41:05 -0500

    feat: loop from config files

--- iso15118/fuzzer/main.py
+++ iso15118/fuzzer/main.py
@@ -2,6 +2,8 @@ import asyncio
 import logging
 import sys
 import time
+from collections import Counter
+from pathlib import Path
 
 import atheris  # type: ignore
 
@@ -24,15 +26,22 @@ secc_config = SECCConfig()
 secc_config.load_envs()
 evcc_config = EVCCConfig()
 evcc_config.load_envs()
-evcc_file_config = load_from_file(evcc_config.ev_config_file_path)
-evcc_file_config.charge_loop_delay_time = 0
+evcc_file_configs = []
+for f in Path("iso15118/shared/examples/evcc").glob("**/*.json"):
+    evcc_file_config = load_from_file(f.__str__())
+    evcc_file_config.charge_loop_delay_time = 0
+    evcc_file_configs.append(evcc_file_config)
 
 
 class EVCCError(Exception):
     pass
 
 
+run_count = 0
 async def main():
+    i = run_count % len(evcc_file_configs)
+    logger.info(f"Running with evcc_file_configs[{i}]")
+    evcc_file_config = evcc_file_configs[i]
     async def run_secc():
         await SECCHandler(
             exi_codec=ExificientEXICodec(),
@@ -56,36 +65,46 @@ async def main():
     await sim_evse_controller.set_status(ServiceStatus.STARTING)
     await asyncio.gather(run_secc(), run_evcc())
 
-run_count = 0
+
+counter = Counter()
+KNOWN_MUTATION_COUNTER = {298: 34105, 338: 20936, 308: 17050, 379: 15356, 469: 1880, 26: 1135, 435: 1038, 23: 852, 385: 852, 386: 852, 393: 847, 394: 847, 31: 847, 11: 847, 12: 847, 332: 847, 175: 377, 176: 377, 54: 377, 459: 94, 111: 94}
+
+
 def run(data: bytes = b""):
     global run_count
     injector = atheris.FuzzInjector()
     l: list[int] = injector.mutation_list
     n = len(l)
+    idx_list = [i for i in range(n)]
+    idx_list.sort(key=lambda x: KNOWN_MUTATION_COUNTER.get(x, 0), reverse=True)
     l.clear()
     fdp = atheris.FuzzedDataProvider(data)
     import random
 
-    random.seed(fdp.ConsumeInt(16))
+    # random.seed(fdp.ConsumeInt(4))
     # random.seed(0)
+    for _ in range(n):
+        l.append(0)
     for i in range(n):
-        if run_count <= 1:
-            value = 0
-        else:
-            value = random.randint(0, 256)
-        l.append(value)
-    logger.info(f"data length: {len(data)}, list length {len(l)} list {l}")
+        if run_count < len(evcc_file_configs):
+            continue
+        l[idx_list[i]] = fdp.ConsumeUInt(1)
+        # l[i] = random.randint(0, 255)
+    logger.info(f"data length: {len(data)}, list length: {len(l)}, mutation list: {l}")
     start_time = time.time()
     logger.info(f"Running main #{run_count}")
-    timeout = 9
+    timeout = 15
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     try:
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
         loop.run_until_complete(asyncio.wait_for(main(), timeout=timeout))
+        counter["normal"] += 1
     except asyncio.TimeoutError:
         logger.error(f"Main function timed out after {timeout} seconds")
+        counter["timeout"] += 1
     except EVCCError as e:
         logger.error(f"EVCCError: {e}")
+        counter[f"evcc_error_{e.args[0].__class__.__name__}"] += 1
     except Exception as e:
         raise e
     finally:
@@ -96,14 +115,22 @@ def run(data: bytes = b""):
         loop.run_until_complete(loop.shutdown_asyncgens())
         loop.close()
         asyncio.set_event_loop(None)
-    logger.info(f"Running main #{run_count} done, Time: {time.time() - start_time:.2f} s")
+    logger.info(
+        f"Running main done #{run_count}, "
+        f"Time: {time.time() - start_time:.2f} s, "
+        f"Counter: {counter}"
+    )
     run_count += 1
+    atheris.FuzzInjector().dump(False, False, False, True)
 
 
 if __name__ == "__main__":
     atheris.FuzzInjector().dump()  # 648 LOAD_FAST, 1124 LOAD_CONST
     if len(sys.argv) > 1 and sys.argv[1] == "--run_once":
-        run()
+        for _ in evcc_file_configs:
+            run()
+    elif len(sys.argv) > 1 and sys.argv[1] == "--run_no":
+        exit(0)
     else:
         atheris.Setup(sys.argv, run)
         atheris.Fuzz()
