86d948d16c4dfdae0252e82d2f9e7dedd037e9b8
BoYanZh <boyanzh233@gmail.com>
2025-02-27 19:08:11 -0500

    chore: config class

--- iso15118/evcc/evcc_config.py
+++ iso15118/evcc/evcc_config.py
@@ -25,28 +25,33 @@ class EVCCConfig:
     _default_supported_energy_services = ["AC"]
 
     def __init__(self, data: dict = None):
-        data = data or {}
+        self.data = data or {}
         self.raw_supported_energy_services: List[str] = data.get(
             "supportedEnergyServices", self._default_supported_energy_services
         )
         self.supported_energy_services: List[ServiceV20] = []
         self.is_cert_install_needed: bool = data.get("isCertInstallNeeded", False)
-        self.use_tls: Optional[bool] = data.get("useTls", True)
-        self.sdp_retry_cycles: Optional[int] = data.get("sdpRetryCycles", 1)
-        self.max_contract_certs: Optional[int] = data.get("maxContractCerts", 3)
+        self.use_tls: bool = data.get("useTls", True)
+        self.sdp_retry_cycles: int = data.get("sdpRetryCycles", 1)
+        self.max_contract_certs: int = data.get("maxContractCerts", 3)
         self.enforce_tls: bool = data.get("enforceTls", False)
         self.raw_supported_protocols: List[str] = data.get(
             "supportedProtocols", self._default_protocols
         )
         self.supported_protocols: List[Protocol] = []
-        self.energy_transfer_mode: Optional[
-            EnergyTransferModeEnum
-        ] = data.get("energyTransferMode", EnergyTransferModeEnum.AC_THREE_PHASE_CORE)
-        self.max_supporting_points: Optional[int] = data.get("maxSupportingPoints", 1024)
-        self.charge_loop_cycle: Optional[int] = data.get("chargeLoopCycle", 10)
-        self.charge_loop_delay_time: Optional[int] = data.get("chargeLoopDelay", 5)
+        self.energy_transfer_mode: EnergyTransferModeEnum = EnergyTransferModeEnum(
+            data.get(
+                "energyTransferMode", EnergyTransferModeEnum.AC_THREE_PHASE_CORE.value
+            )
+        )
+        self.max_supporting_points: int = data.get("maxSupportingPoints", 1024)
+        self.charge_loop_cycle: int = data.get("chargeLoopCycle", 10)
+        self.charge_loop_delay_time: int = data.get("chargeLoopDelay", 5)
 
         self.load_raw_values()
+
+    def __str__(self):
+        return json.dumps(self.data)
 
     def load_raw_values(self):
         self.supported_energy_services = load_requested_energy_services(
